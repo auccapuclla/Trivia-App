3b5b49fcc855646e66e6ee851d7a31bdc5e487b6
Joel Auccapuclla <auccapuclla2013@gmail.com>
2022-03-31 21:25:13 +0800

    css added. pause and resume button added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useTrivias } from "./utils/getTrivias";
+import './App.css'
 
 const Choices = ({ correctAnswer, incorrectAnswers, handleNext }) => {
   const checkHandle = (answer) => {
@@ -11,7 +12,6 @@ const Choices = ({ correctAnswer, incorrectAnswers, handleNext }) => {
     }
 
     handleNext(false);
-    console.log("false");
   };
 
   const options = [correctAnswer, ...incorrectAnswers].sort(
@@ -19,55 +19,65 @@ const Choices = ({ correctAnswer, incorrectAnswers, handleNext }) => {
   );
 
   return (
-    <>
+    <ul className="options">
       {options.map((option, id) => (
-        <button key={id} onClick={() => checkHandle(option)}>
+        <li className="option"> <button className="button_option" key={id} onClick={() => checkHandle(option)}>
           {option}
         </button>
+        </li>
       ))}
-    </>
+    </ul>
   );
 };
 
-const Trivia = ({ play, trivias: defaultTrivias }) => {
+const Trivia = ({ play, setPlay, fetchTrivias, trivias: defaultTrivias }) => {
   const [trivias, setTrivias] = useState([]);
   const [corrects, setCorrects] = useState(0);
-
+  const [resume, setResume] = useState(true);
+  
   const handleNext = (isCorrect) => {
     if (isCorrect) setCorrects((prev) => prev + 1);
-
     const newTrivias = [...trivias];
     newTrivias.shift();
-
     setTrivias(newTrivias);
   };
-
+  const handleResumeClick = () => {
+    setResume(!resume);
+  };
+  const handleReloadClick = () => {
+    fetchTrivias();
+  };
   useEffect(() => {
     setTrivias(defaultTrivias);
     setCorrects(0);
   }, [defaultTrivias]);
-
+  const handleNewGame = () => setPlay(!play)
   if (play) {
     if (trivias.length) {
       const { correct_answer, incorrect_answers } = trivias[0];
       const question = trivias[0].question;
-
       return (
         <>
-          <div>{question}</div>
-
-          <Choices
-            correctAnswer={correct_answer}
-            incorrectAnswers={incorrect_answers}
-            handleNext={handleNext}
-          />
+          <button onClick={handleResumeClick} className="button">{resume ? "Pause" : "Resume"}</button>
+          <button onClick={handleReloadClick} className="button">Reload game</button> 
+          {resume?
+            <>
+              <div>{question}</div>
+              <Choices
+                correctAnswer={correct_answer}
+                incorrectAnswers={incorrect_answers}
+                handleNext={handleNext}
+              /> 
+            </>:
+            null
+          }
         </>
       );
     }
-
     if (corrects) {
       return (
         <>
+          <button onClick={handleNewGame} className="button">Start New</button>
           <h1>Game over</h1>
           <h2>
             You have scored {corrects} out of {defaultTrivias.length} trivias
@@ -75,34 +85,26 @@ const Trivia = ({ play, trivias: defaultTrivias }) => {
         </>
       );
     }
-
     return null;
   }
-
   return null;
 };
 
 const App = () => {
   const [play, setPlay] = useState(false);
-  const [trivias, fetchTrivias] = useTrivias();
-
-  const handlePlayClick = () => {
+  const [trivias, fetchTrivias] = useTrivias();  
+  
+  const handleStartGame = () => {
+    fetchTrivias();
     setPlay(!play);
-    fetchTrivias();
   };
-
-  const handleReloadClick = () => {
-    fetchTrivias();
-  };
-
   return (
     <div>
       <h1> Try your best on this Trivia Challenge </h1>
-
-      <button onClick={handlePlayClick}>{play ? "Pause" : "Play"}</button>
-      <button onClick={handleReloadClick}>Reload game</button>
-
-      <Trivia play={play} trivias={trivias} />
+      { !play ? 
+        <button onClick={handleStartGame} className="button">Start</button> :  
+        <Trivia play={play} setPlay={setPlay} trivias={trivias} fetchTrivias={fetchTrivias} />
+      }
     </div>
   );
 };
